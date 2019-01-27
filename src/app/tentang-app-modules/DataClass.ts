727351af98cc2b7c3bfc79782f7f2fca8bf31336
GulajavaMinistudio <gulajava.mini@gmail.com>
2019-01-27 17:03:33 +0700

    Pembaruan versi paket

---
+++ src/app/tentang-app-modules/DataClass.ts
@@ -0,0 +1,15 @@
+export default class DataClass {
+
+  data = '';
+
+  constructor() {
+    this.data = 'hello world';
+  }
+
+  testVariableJSTS() {
+
+    const foo = {bar: 0};
+    foo.bar = 42;
+    console.log(foo);
+  }
+}
